9c6d9451110c38113fc241a0530f8686e15db541
Gyanashree <gyanashree.skaplink@gmail.com>
2023-10-15 22:58:47 +0530

    ignite our app

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
-       /**  <div id="parent">
-            <div id="child">
-                <h1>h1 tag</h1>
-                <h2>h2 tag</h2>
-
-            </div>
-
-        </div> */
-        
-        
-        
-        
+       import React from 'react';
+       import  ReactDOM  from 'react-dom/client';
         
         
         const heading = React.createElement("div", { id: "parent"} ,     // core react
                         React.createElement("div", { id: "child"} ,
                         [React.createElement("h1", { } ,
-                        "H1 tag"),
+                        "H1 tag hello"),
                         React.createElement("h2", { } ,
                         "H2 Tag")]));
 
---
+++ README.md
@@ -0,0 +1,23 @@
+//
+# parcel advantage ---
+-dev build
+- local server
+- HMR - hot module replacement
+- file watching algo written by c++
+- caching - faster builds
+- image optimization
+- compressing
+- minification
+- bundling
+- consistent hashing
+- code splitting
+- differential bundling
+- Diagnostic
+- Error Handling
+- https
+- tree shaking - remove unused code
+
+
+
+ # note 
+ [ "main": "App.js" ] remove this line from package.json when build the app ( npx parcel build index.html)
